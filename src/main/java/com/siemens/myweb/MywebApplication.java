0dd77820ef99a971efd9a3e25814cb3c41aa8753
Nilesh Devdas <nilesh.devdas@vinsys.com>
2019-09-10 14:44:49 +0530

    Initial Commit

---
+++ src/main/java/com/siemens/myweb/MywebApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebApplication.class, args);
+	}
+
+}
